e3a3ee3533e42ef3927c26a5f7a0b31195c9afeb
gaoge <314013991@qq.com>
2023-12-26 19:31:09 +0800

    mint页开发save

--- src/components/PageTwo.vue
+++ src/components/PageTwo.vue
@@ -4,6 +4,7 @@ import TabBar from './TabBar.vue'
 import { event } from '../utils/event-bus'
 // import tippy from 'tippy.js';
 import { preloadImg } from '../utils/preload'
+import { nftList as tabViewDataList } from '../const'
 
 const viewImg1 = 'https://static.demr.xyz/assets/card1-zxjo_0rL.png'
 const viewImg2 = 'https://static.demr.xyz/assets/card2-9-hKOMn3.png'
@@ -13,26 +14,6 @@ const viewImg3 = 'https://static.demr.xyz/assets/card3-rA3YVBqM.png'
 
 const tabActiveIndex = ref(0)
 
-const tabViewDataList = [
-  {
-    title: 'Compass NFT',
-    content: 'Compass is the core NFT in DeMR, which is the necessary identification for contributors, who can participate in the construction of DeMR high-precision MR maps and receive DMR token rewards. Compass NFT will be limited release in stages on the official website.',
-    tip: 'Holding equity:  Token airdrops, Staking, and Scan to Earn',
-    img: viewImg1
-  },
-  {
-    title: 'Node License NFT',
-    content: 'DeMR construction requires substantial spatial computation, real-time perception, and interactive rendering, necessitating numerous computing and storage units. To become a DeMR Computing Node, computing suppliers must hold the Node License NFT, which will be limited release in stages on the official website. ',
-    tip: 'Holding equity: Token airdrops, Staking, and Compute to Earn',
-    img: viewImg2
-  },
-  {
-    title: 'Land NFT',
-    content: "DeMR's Land NFT symbolizes Web3 Spatial Internet infrastructure rights and real-world scarce asset, serving as a potential business right for future MR content ecosystem development. ",
-    tip: 'Holding equity: Token airdrops, Land equity',
-    img: viewImg3
-  },
-]
 
 // const currentViewData = computed(() => {
 //   return tabViewDataList[tabActiveIndex.value];
@@ -53,8 +34,8 @@ event.on('goToNft', () => {
 
 onMounted(() => {
   setTimeout(() => {
-    preloadImg(viewImg2)
-    preloadImg(viewImg3)
+    preloadImg(tabViewDataList[1].img)
+    preloadImg(tabViewDataList[2].img)
   }, 1500)
 })
 </script>
--- src/views/MintView.vue
+++ src/views/MintView.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+import { computed, type Ref } from "vue";
+import { nftList, type NFTData } from "../const"
+import { useRoute } from "vue-router"
+
 // import { WalletMultiButton } from "solana-wallets-vue";
 function haveOKX() {
   // @ts-ignore
   if (typeof window.okxwallet !== 'undefined') {console.log('OKX is installed!');}
 }
+
+const route = useRoute()
+
+const nftData:Ref<NFTData> = computed(() => {
+  const queryType = route.query?.type as string
+  const data = nftList[+queryType - 1]
+  return data
+})
 
 async function connect() {
   try {
@@ -21,17 +33,61 @@ async function connect() {
 </script>
 
 <template>
-  <div class="about">
-    <button @click="connect">connect</button>
+  <div class="mint-page">
+    <div class="page-main">
+      <div class="nft-info">
+        <div class="nft-name">{{ nftData.title }}</div>
+        <img class="nft-img" :src="nftData.img" />
+        <div class="nft-content">{{ nftData.content }}</div>
+        <div class="nft-tip">{{ nftData.tip }}</div>
+      </div>
+      <div class="mint-box"></div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
+<style scoped>
+.mint-page {
+  background-color: #000;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  .page-main {
+    color: #fff;
     display: flex;
-    align-items: center;
+  }
+
+  .nft-info {
+
+  }
+}
+@media (min-width: 1024px) {
+  .mint-page {
+    .page-main {
+      max-width: 1200PX;
+      display: flex;
+      align-items: center;
+    }
+    .nft-name {
+      font-size: 24PX;
+      font-weight: 900;
+    }
+    .nft-img {
+      width: 480PX;
+    }
+
+    .nft-content {
+      width: 500PX;
+      color: #888;
+      font-size: 16PX;
+    }
+
+    .nft-tip {
+      color: #D7D7D7;
+      font-size: 16PX;
+      margin-top: 20PX;
+    }
+
   }
 }
 </style>
